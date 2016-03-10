18f0491809451ad851e6815c1410856c6cf3d0bf
Exac <contact@thomasmclennan.ca>
2016-03-10 01:33:37 -0800

    Setup

---
+++ PHP/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Interface Selection | Chocoholics Anonymous</title>
+    <meta name="description" content="Chocoholics Anonymous">
+    <meta name="author" content="Chocoholics Anonymous interface selection.">
+    <link rel="stylesheet" href="/cdn/css/style.css">
+</head>
+
+<body id="interfacepicker">
+	<h1 class="logo" onclick="document.location = '/'">Chocoholics Anonymous</h1>
+	<ul class="flex-container">
+		<li class="flex-item"><a href="/manager/">Manager</a></li>
+		<li class="flex-item"><a href="/operator/">Operator</a></li>
+		<li class="flex-item"><a href="/provider/">Provider</a></li>
+	</ul>
+	<p id="footer">&copy; <script>document.write(new Date().getFullYear());</script> Farzin Dhanji, Navi Gill  &amp; Thomas McLennan</p>
+</body>
+</html>
---
+++ PHP/operator/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas
+ * Date: 3/4/16
+ * Time: 15:00
+ */
+
+require_once(dirname(dirname(__FILE__)).'/Main.class.php');
+
+new Main("operator");
