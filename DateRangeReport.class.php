1d24411557e1871a56251e54f24f088554c2e227
navigill2511 <navigill.2511@gmail.com>
2016-03-10 17:17:26 -0800

    Added on march 10th by Navi

---
+++ DateRangeReport.class.php
@@ -0,0 +1,51 @@
+<?php 
+
+/**
+ * Report
+ *
+ * 10-03-2016
+ *
+ */
+class DateRangeReport extends Report
+{
+	private $endDate;
+	private $endDateRange;
+	private $startDateRange;
+	
+	const DATE_FORMAT = "String";
+	const DATE_TIME_FORMAT = "String";
+	const dateFormatter = "";
+	const dateTimeFormatter = "";
+	
+	public function __construct()
+	{
+		
+	}
+	
+	public function getEndDate()
+	{
+		
+	}
+	
+	public function getEndDateRange()
+	{
+		
+	}
+	
+	public function getStartDateRange()
+	{
+		
+	}
+	
+	public function setDates()
+	{
+		
+	}
+	
+	public function sendByEmail()
+	{
+		
+	}
+	
+	
+}
---
+++ Report.class.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Report
+ *
+ * 10-03-2016
+ *
+ */
+class Report 
+{
+	private $sb;
+	private $fileName;
+	private $formatter;
+	
+	const currencyFormatter = "";
+	
+	public function __construct()
+	{
+		
+	}
+	
+	public function addHeadding()
+	{
+		
+	}
+	
+	public function addErrorMessage()
+	{
+		
+	}
+	
+	public function sendByEmail()
+	{
+		
+	}
+	
+	public function display()
+	{
+		
+	}
+	
+	public function print()
+	{
+		
+	}
+	
+	public function print()
+	{
+		
+	}
+	
+	public function getFileName()
+	{
+		
+	}
+	
+	
+	
+	
+}
