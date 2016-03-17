0ead26b9ac4daab92fc831626e3906aed0069747
Exac <contact@thomasmclennan.ca>
2016-03-17 00:39:20 -0700

    Update Readme.md. Example form in UserInterface.

--- UserInterface.class.php
+++ UserInterface.class.php
@@ -79,8 +79,8 @@ class UserInterface
 	{
 		
 	}
-	
-	public function promtForLong()
+
+	public function promptForLong()
 	{
 		
 	}
@@ -130,3 +130,76 @@ class Button
 	}
 
 }
+
+class Input
+{
+	public $name = null;
+	public $label = null;
+	public $type = null;
+	public $radiogroup = "default";
+
+	/**
+	 * @param $_type button|checkbox|radio|text|password
+	 * @param $_name
+	 * @param $_label
+	 */
+	public function __construct($_type, $_name, $_label)
+	{
+		$this->type = $_type;
+		$this->name = $_name;
+		$this->label = $_label;
+
+		//If an optional 4th parameter is passed, use it to determine a radio button's group.
+		if (func_num_args() === 4) {
+			$this->radiogroup = func_get_args()[3];
+		}
+	}
+
+	public function __toString()
+	{
+		if ($this->type === "button" || $this->type === "submit") {
+			$o = '<button type="' . $this->type . '" id="' . $this->getId($this->name) . '" name="'
+				. $this->getId($this->name) . '">' . $this->name . '</button>';
+		} else if ($this->radiogroup !== "radio") {
+			$o = '<input type="' . $this->type . '" id="' . $this->getId($this->name) . '" name="'
+				. $this->radiogroup . '"/>';
+		} else {
+			$o = '<input type="' . $this->type . '" id="' . $this->getId($this->name) . '" name="'
+				. $this->getId($this->name) . '"/>';
+		}
+
+		$o .= '<label for="' . $this->getId($this->name) . '">' . $this->label . '</label>';
+
+		return $o;
+	}
+
+	public function br()
+	{
+		return $this->__toString() . "<br>";
+	}
+
+	/**
+	 * @desc creates a html-id-safe name for variable. "nice Button" becomes "nice_Button"
+	 */
+	public function getId($_name)
+	{
+		return str_replace(" ", "_", $_name);
+	}
+
+}
+
+/*
+ * Form Example
+ */
+/*
+$h = new Html();
+$h->body .= "<form action='' method='get'>";
+$h->body .= (new Input("button", "lad", "Click for lad"))->br();
+$h->body .= (new Input("text", "wew", "Enter name lad"))->br();
+$h->body .= (new Input("checkbox", "amaze", "amaze wew"))->br();
+$h->body .= (new Input("radio", "so", "doge amaze", "ohvey"))->br();
+$h->body .= (new Input("radio", "doge", "wow", "ohvey"))->br();
+$h->body .= (new Input("submit", "oh", "vey"))->br();
+$h->body .= "</form>";
+new Input("radio", "test", "this is a test", "wew");
+echo $h;*/
--- readme.md
+++ readme.md
@@ -1,2 +1,24 @@
-# Server
-The server runs these files, and outputs the interfaces in HTML.
+
+#14.16
+Starting with your specifications of Problem 12.20 or 13.22, design the Chocoholics Anonymous product (Appendix A). Use the 9-step process.
+##Design Descisions
+1.	An HTML GUI will be used to create the provider, operator & manager interface.
+2.	Reports will be saved as html files.
+3.	ETF data will be saved to html files.
+4.	The system will be implemented in three parts:
+a.	A server running Apache2 (object-oriented PHP)
+b.	A server running a MySql database holding the claim, member, provider and, service tables.
+c.	An arbitrary computing device to connect to the web server.
+5.	The operator interface allows updating of a member’s data.
+6.	The accounting (Scheduler) can be run manually, or with a cron job every Friday.
+7.	Persistent class data is stored in the database, and accessed on run.
+8.	Relationships between entity classes (Claim, Service, Provider and, Member) are implemented by identifying indexes stored in the database.
+9.	The server-side code will be implemented in PHP and shell scripts. The client-side code will run JavaScript. The database server is MySql.
+
+##Mapping of Analysis Classes to Design Classes
+Analysis classes map to design classes.
+The **ChocAnSystem** class creates the provider (terminal?), operator and, manager interfaces.
+The **UserInterface** class handles the four user interfaces (**ProviderInterface**, **OperatorInterface**, **ManagerInterface** and, **SchedulerInterface**. They are used for all output.
+The entity classes have their information stored in the database. Each entry holds the data for one entity class (**Members**, **Providers**, **Persons**, **Services** and **Claims**).
+**ProviderMaintainer** and **MemberMaintainer** inherit **PersonMaintainer**.
+
