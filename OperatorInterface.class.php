323d201c8b78b635608084629564bfe4ea3266ba
Exac <tgmclennan@hotmail.com>
2016-03-17 16:47:48 -0700

    Test form

--- OperatorInterface.class.php
+++ OperatorInterface.class.php
@@ -21,6 +21,9 @@ class OperatorInterface
 		$this->ui->bodyId = "operatorinterface";
 		$this->ui->body .= Utils::getNavigationMenu();
 
+		$this->ui->body .= "<form method='post' action=''>";
+		$this->ui->body .= (new Button("test button", "This is the test button"));
+		$this->ui->body .= "</form>";
 		echo $this->ui;
 	}
 }
