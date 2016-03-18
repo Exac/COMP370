c1ed3ab2f122559972e49e312b25534ba4da6ebc
navigill2511 <navigill.2511@gmail.com>
2016-03-18 14:59:36 -0700

    test

--- operator/index.php
+++ operator/index.php
@@ -18,17 +18,42 @@
 
         <fieldset>
             <legend>Add Member</legend>
-            Name: <br><input type="text" name="name"><br>
-            Street address: <br><input type="text" name="street_address"><br>
-            City: <br><input type="text" name="city"><br>
-            Province: <br><input type="text" name="province"><br>
-            Postal code: <br><input type="text" name="postalCode"><br>
-            E-mail: <br><input type="email" name="email"><br>
-            Status: <br><input type="radio" name="status" value="ACTIVE"> Active
-                    <br><input type="radio" name="status" value="SUSPENDED"> Suspended<br><br>
+            Name:           <br><input type="text"  name="name"><br>
+            Street address: <br><input type="text"  name="street_address"><br>
+            City:           <br><input type="text"  name="city"><br>
+            Province:       <br><input type="text"  name="province"><br>
+            Postal code:    <br><input type="text"  name="postalCode"><br>
+            E-mail:         <br><input type="email" name="email"><br>
+            Status:         <br><input type="radio" name="status" value="ACTIVE"> Active
+                            <br><input type="radio" name="status" value="SUSPENDED"> Suspended
+                            <br><br>
+
             <button type="submit" id="addMembers">add_member</button>
         </fieldset>
     </form>
+
+<form id="providerControls" action="" method="post">
+    <fieldset>
+        <legend>View Providers</legend>
+        <button type="button" id="viewMembers">view_members</button>
+    </fieldset>
+
+    <fieldset>
+        <legend>Add Provider</legend>
+        Name:           <br><input type="text"  name="name"><br>
+        Street address: <br><input type="text"  name="street_address"><br>
+        City:           <br><input type="text"  name="city"><br>
+        Province:       <br><input type="text"  name="province"><br>
+        Postal code:    <br><input type="text"  name="postalCode"><br>
+        E-mail:         <br><input type="email" name="email"><br>
+        Status:         <br><input type="radio" name="type" value="DIETITIAN"> Dietitian
+                        <br><input type="radio" name="type" value="INTERNIST"> Internist
+                        <br><input type="radio" name="type" value="EXERCISE_EXPERT"> Exercise expert
+                        <br><br>
+
+        <button type="submit" id="addMembers">add_member</button>
+    </fieldset>
+</form>
 </html>
 
 <?php
