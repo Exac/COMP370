489a282546aec632ffc1dd855199e09d1ca731a7
navigill2511 <navigill.2511@gmail.com>
2016-03-18 14:31:38 -0700

    test

--- operator/index.php
+++ operator/index.php
@@ -1,3 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Operator | Chocoholics Anonymous</title>
+</head>
+
+<body id="operator" class="interface">
+
+</body>
+    <form id="memberControls" action="" method="post">
+        <fieldset>
+            <legend>View Members</legend>
+            <button type="button" id="viewMembers">view_members</button>
+        </fieldset>
+
+        <fieldset>
+            <fieldset>
+                <legend>Add Members</legend>
+                Name: <input type="text" name="member_name"><br>
+                Street address: <input type="text" name="member_street_address"><br>
+                E-mail: <input type="text" name="email"><br>
+                <button type="submit" id="addMembers">add_members</button>
+            </fieldset>
+        </fieldset>
+    </form>
+</html>
+
 <?php
 /**
  * Created by PhpStorm.
