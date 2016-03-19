2b5b3648d06f12a7b9ba4995b21f0dd3aff1efda
navigill2511 <navigill.2511@gmail.com>
2016-03-18 18:15:33 -0700

    operator interface, something like this??

--- operator/index.php
+++ operator/index.php
@@ -21,24 +21,24 @@
         <legend>New Person</legend>
 
         <input type="radio" name="person" value="member">Member
-        <input type="radio" name="person" value="provider">Provider
+        <input type="radio" name="person" value="provider">Provider<br><br>
 
         Name:           <br><input type="text"  name="name"><br>
         Street address: <br><input type="text"  name="street_address"><br>
         City:           <br><input type="text"  name="city"><br>
         Province:       <br><input type="text"  name="province"><br>
         Postal code:    <br><input type="text"  name="postalCode"><br>
-        E-mail:         <br><input type="email" name="email"><br>
+        E-mail:         <br><input type="email" name="email"><br><br>
         Member Status:  <br><input type="radio" name="status" value="ACTIVE"> Active
                         <br><input type="radio" name="status" value="SUSPENDED"> Suspended
-                        <br>
+                        <br><br>
 
         Provider Type:  <br><input type="radio" name="type" value="DIETITIAN"> Dietitian
                         <br><input type="radio" name="type" value="INTERNIST"> Internist
                         <br><input type="radio" name="type" value="EXERCISE_EXPERT"> Exercise expert
                         <br><br>
 
-        <button type="submit" id="addPerson">add_member</button>
+        <button type="submit" id="addPerson">add_person</button>
     </fieldset>
 </form>
 
