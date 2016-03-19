38d016a30a200bd2c2d7afb2e1bee198460ec8ed
navigill2511 <navigill.2511@gmail.com>
2016-03-18 18:25:04 -0700

    operator interface, something like this??

--- operator/index.php
+++ operator/index.php
@@ -11,13 +11,13 @@
 
 </body>
 <form id="memberControls" action="../OperatorInterface.class.php" method="POST">
-    <fieldset style="width: 250px;">
+    <fieldset style="width: 500px;">
         <legend>View Members</legend>
         <label>click_to_view_members</label>
         <button type="button" id="viewMembers">view_members</button>
     </fieldset><br>
 
-    <fieldset>
+    <fieldset style="width: 500px;">
         <legend>New Person</legend>
 
         <input type="radio" name="person" value="member">Member
@@ -30,14 +30,14 @@
         Postal code:    <br><input type="text"  name="postalCode"><br>
         E-mail:         <br><input type="email" name="email"><br><br>
 
-        <fieldset><legend>Fill only for member</legend>
+        <fieldset style="width: 500px;"><legend>Fill only for member</legend>
 
         Member Status:  <br><input type="radio" name="status" value="ACTIVE"> Active
                         <br><input type="radio" name="status" value="SUSPENDED"> Suspended
                         <br><br>
         </fieldset><br>
 
-        <fieldset><legend>Fill only for provider</legend>
+        <fieldset style="width: 500px;"><legend>Fill only for provider</legend>
         Provider Type:  <br><input type="radio" name="type" value="DIETITIAN"> Dietitian
                         <br><input type="radio" name="type" value="INTERNIST"> Internist
                         <br><input type="radio" name="type" value="EXERCISE_EXPERT"> Exercise expert
