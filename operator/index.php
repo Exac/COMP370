e77fc842200305651eec3fdf2f3d46ff63a2521b
navigill2511 <navigill.2511@gmail.com>
2016-03-18 17:29:49 -0700

    operator interface, something like this??

--- operator/index.php
+++ operator/index.php
@@ -28,7 +28,7 @@
                         <br><input type="radio" name="status" value="SUSPENDED"> Suspended
                         <br><br>
 
-        <input type="submit" id="addMember">add_member</input>
+        <button type="submit" id="addMember">add_member</button>
     </fieldset>
 </form>
 
@@ -51,7 +51,7 @@
                         <br><input type="radio" name="type" value="EXERCISE_EXPERT"> Exercise expert
                         <br><br>
 
-        <input type="submit" id="addProvider">add_member</input>
+        <button type="submit" id="addProvider">add_member</button>
     </fieldset>
 </form>
 </html>
