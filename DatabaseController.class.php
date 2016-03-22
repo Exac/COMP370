93214fdfe86230719ab65239c6d0f68de7b982b2
Exac <contact@thomasmclennan.ca>
2016-03-22 00:38:54 -0700

    More Provider commits.

--- DatabaseController.class.php
+++ DatabaseController.class.php
@@ -104,14 +104,6 @@ class DatabaseController
 	 */
 	public static function selectMember($member_number)
 	{
-//		self::initialize();
-//
-//		$member_number = mysql_real_escape_string($member_number);
-//		$rows = self::$db->select("SELECT * FROM member where member_number = ${member_number}");
-//
-//		foreach($rows as &$r){
-//			return $r;
-//		}
 		self::initialize();
 
 		$member_number = mysql_real_escape_string($member_number);
@@ -149,4 +141,19 @@ class DatabaseController
 		return self::$db->select("select Auto_increment FROM information_schema.tables where table_name='${table}'")[0]["Auto_increment"];
 	}
 
+	public static function memberExists($member_number)
+	{
+		self::initialize();
+
+		$rows = self::$db->select("SELECT member_number FROM member WHERE member_number = " . $member_number);
+		foreach ($rows as &$row)
+		{
+			if (isset($row["member_number"]))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
